fe490df06f223215d1e70a35a865ae514491260b
mkanamaru <kanamaru-masanori@hotmail.co.jp>
2025-05-07 11:47:27 +0200

    Add `BoundingBox`

--- src/ray_intersection.jl
+++ src/ray_intersection.jl
@@ -3,8 +3,12 @@
 
 小惑星の形状モデルに対するレイと表面の交差判定処理を実装する。
 Möller–Trumbore法を用いたレイと三角形メッシュの交差判定を提供する。
+バウンディングボックスを用いた高速化機能も提供する。
 """
 
+#---------------------------------------------------------------------#
+#          Basic structure for intersection determination             #
+#---------------------------------------------------------------------#
 
 """
     Ray
@@ -42,6 +46,175 @@ function Base.show(io::IO, ray::Ray)
         ∘ direction = $(ray.direction)
     """
     print(io, msg)
+end
+
+"""
+    BoundingBox
+
+形状モデルのバウンディングボックスを表す構造体。
+
+# フィールド
+- `min_point` : バウンディングボックスの最小点（x, y, zの最小値）
+- `max_point` : バウンディングボックスの最大点（x, y, zの最大値）
+"""
+struct BoundingBox
+    min_point::SVector{3, Float64}
+    max_point::SVector{3, Float64}
+end
+
+"""
+    Base.show(io::IO, bbox::BoundingBox)
+
+BoundingBoxオブジェクトを表示するための関数。
+
+# 引数
+- `io`   : 出力ストリーム
+- `bbox` : 表示するBoundingBoxオブジェクト
+"""
+function Base.show(io::IO, bbox::BoundingBox)
+    msg = """
+    BoundingBox:
+        ∘ min_point = $(bbox.min_point)
+        ∘ max_point = $(bbox.max_point)
+    """
+    print(io, msg)
+end
+
+"""
+    compute_bounding_box(shape::AsteroidThermoPhysicalModels.ShapeModel) -> BoundingBox
+
+形状モデルのバウンディングボックスを計算する。
+
+# 引数
+- `shape` : 形状モデル
+
+# 戻り値
+- バウンディングボックスを表す`BoundingBox`オブジェクト
+"""
+function compute_bounding_box(shape::AsteroidThermoPhysicalModels.ShapeModel)
+    # 初期値を設定
+    min_x, min_y, min_z =  Inf,  Inf,  Inf
+    max_x, max_y, max_z = -Inf, -Inf, -Inf
+    
+    # 全ての頂点を走査して最小値と最大値を更新
+    for node in shape.nodes
+        min_x = min(min_x, node[1])
+        min_y = min(min_y, node[2])
+        min_z = min(min_z, node[3])
+        
+        max_x = max(max_x, node[1])
+        max_y = max(max_y, node[2])
+        max_z = max(max_z, node[3])
+    end
+    
+    # バウンディングボックスを作成
+    min_point = SVector{3, Float64}(min_x, min_y, min_z)
+    max_point = SVector{3, Float64}(max_x, max_y, max_z)
+    
+    return BoundingBox(min_point, max_point)
+end
+
+#---------------------------------------------------------------------#
+#                     Intersection deteremination                     #
+#---------------------------------------------------------------------#
+
+"""
+    intersect_ray_bounding_box(ray::Ray, bbox::BoundingBox) -> Bool
+
+レイとバウンディングボックスの交差判定を行う。
+
+# 引数
+- `ray`  : レイ
+- `bbox` : バウンディングボックス
+
+# 戻り値
+- 交差する場合はtrue、しない場合はfalse
+"""
+function intersect_ray_bounding_box(ray::Ray, bbox::BoundingBox)
+    # スラブ法を使用してレイとバウンディングボックスの交差判定を行う
+    t_min = -Inf
+    t_max = Inf
+    
+    # x軸方向の交差判定
+    if abs(ray.direction[1]) < 1e-8
+        # レイがx軸に平行で、レイの始点がバウンディングボックスの外側にある場合
+        if ray.origin[1] < bbox.min_point[1] || ray.origin[1] > bbox.max_point[1]
+            return false
+        end
+    else
+        # x軸方向の交差パラメータを計算
+        t1 = (bbox.min_point[1] - ray.origin[1]) / ray.direction[1]
+        t2 = (bbox.max_point[1] - ray.origin[1]) / ray.direction[1]
+        
+        # t1とt2を並べ替え
+        if t1 > t2
+            t1, t2 = t2, t1
+        end
+        
+        # 交差範囲を更新
+        t_min = max(t_min, t1)
+        t_max = min(t_max, t2)
+        
+        # 交差範囲が無効になった場合
+        if t_min > t_max
+            return false
+        end
+    end
+    
+    # y軸方向の交差判定
+    if abs(ray.direction[2]) < 1e-8
+        # レイがy軸に平行で、レイの始点がバウンディングボックスの外側にある場合
+        if ray.origin[2] < bbox.min_point[2] || ray.origin[2] > bbox.max_point[2]
+            return false
+        end
+    else
+        # y軸方向の交差パラメータを計算
+        t1 = (bbox.min_point[2] - ray.origin[2]) / ray.direction[2]
+        t2 = (bbox.max_point[2] - ray.origin[2]) / ray.direction[2]
+        
+        # t1とt2を並べ替え
+        if t1 > t2
+            t1, t2 = t2, t1
+        end
+        
+        # 交差範囲を更新
+        t_min = max(t_min, t1)
+        t_max = min(t_max, t2)
+        
+        # 交差範囲が無効になった場合
+        if t_min > t_max
+            return false
+        end
+    end
+    
+    # z軸方向の交差判定
+    if abs(ray.direction[3]) < 1e-8
+        # レイがz軸に平行で、レイの始点がバウンディングボックスの外側にある場合
+        if ray.origin[3] < bbox.min_point[3] || ray.origin[3] > bbox.max_point[3]
+            return false
+        end
+    else
+        # z軸方向の交差パラメータを計算
+        t1 = (bbox.min_point[3] - ray.origin[3]) / ray.direction[3]
+        t2 = (bbox.max_point[3] - ray.origin[3]) / ray.direction[3]
+        
+        # t1とt2を並べ替え
+        if t1 > t2
+            t1, t2 = t2, t1
+        end
+        
+        # 交差範囲を更新
+        t_min = max(t_min, t1)
+        t_max = min(t_max, t2)
+        
+        # 交差範囲が無効になった場合
+        if t_min > t_max
+            return false
+        end
+    end
+    
+    # 交差範囲が有効な場合
+    return t_max >= 0.0
 end
 
 """
@@ -207,20 +380,27 @@ function Base.show(io::IO, result::RayShapeIntersectionResult)
     end
 end
 
-
 """
-    intersect_ray_shape(ray::Ray, shape::AsteroidThermoPhysicalModels.ShapeModel) -> RayShapeIntersectionResult
+    intersect_ray_shape(ray::Ray, shape::AsteroidThermoPhysicalModels.ShapeModel, bbox::BoundingBox) -> RayShapeIntersectionResult
 
-Möller–Trumbore法を用いてレイと形状モデルの交差判定を行う。
+バウンディングボックスを使用して高速化されたレイと形状モデルの交差判定を行う。
+Möller–Trumbore法を用いてレイと三角形メッシュの交差判定を行う。
 
 # 引数
 - `ray`   : レイ
 - `shape` : 形状モデル
+- `bbox`  : 形状モデルのバウンディングボックス
 
 # 戻り値
 - 交差判定結果を格納した`RayShapeIntersectionResult`オブジェクト
 """
-function intersect_ray_shape(ray::Ray, shape::AsteroidThermoPhysicalModels.ShapeModel)
+function intersect_ray_shape(ray::Ray, shape::AsteroidThermoPhysicalModels.ShapeModel, bbox::BoundingBox)
+    # まずバウンディングボックスとの交差判定を行う
+    if !intersect_ray_bounding_box(ray, bbox)
+        return NO_INTERSECTION_RAY_SHAPE
+    end
+    
+    # バウンディングボックスと交差する場合は、詳細な交差判定を行う
     min_distance   = Inf
     closest_point  = @SVector zeros(3)
     hit_face_index = 0
